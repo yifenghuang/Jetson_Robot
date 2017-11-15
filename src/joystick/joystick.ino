fc08c91da9c8ed0eef93494c3b5b93ad53e25701
hyf <yifenghuang.dh@gmail.com>
2017-11-15 12:26:38 +0800

    add local mode

--- src/joystick/joystick.ino
+++ src/joystick/joystick.ino
@@ -3,17 +3,30 @@
 #include <rosserial_arduino/Adc.h>
 #include <std_msgs/String.h>
 
+//#define REMOTE //ROS control arduino mode. huge delay
+
 ros::NodeHandle nh;
 
 rosserial_arduino::Adc adc_msg;
 std_msgs::String str_msg;
 ros::Publisher chatter("chatter", &str_msg);
 
+String lefthead="mov pleft=";
+String righthead="mov pright=";
+String huiche="\n";
+float Throttle=0;
+float Steering=0;
+
 void setup()
 {
+#ifdef REMOTE  
    pinMode(13, OUTPUT);
    nh.initNode();
    nh.advertise(chatter);
+#else   
+   Serial.begin(115200);
+   while(Serial.read()>= 0){}//clear serialbuffer  
+#endif
 }
  
  //average the analog reading to eliminate some of the noise
@@ -24,15 +37,19 @@ int averageAnalog(int pin){
 }
 
 char ad1[13] = "";
+char ad2[13] = "";
 
 char* int2str(char *str, int i)
 {
   sprintf(str,"%d",i);
   return str;
 }
+
+
  
 void loop()
 {
+#ifdef REMOTE
      //adc_msg.adc0 = analogRead(0);
      //adc_msg.adc1 = analogRead(1);
      dtostrf((analogRead(0)-505)/100.0,1,2,ad1);
@@ -40,4 +57,13 @@ void loop()
      chatter.publish(&str_msg);
      nh.spinOnce();
      delay(5);
+#else
+     Throttle = (analogRead(0)-505)/100.0;
+     Steering = (analogRead(1)-505)/150.0;
+     dtostrf(Throttle-Steering,1,2,ad1);
+     dtostrf(Throttle+Steering,1,2,ad2);
+     Serial.println(lefthead+ad1);
+     Serial.println(righthead+ad2);   
+     delay(5);
+#endif
 }
