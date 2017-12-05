ef3f201b01d4e767a405472d8841abf64c3b5424
hyf <yifenghuang.dh@gmail.com>
2017-12-05 09:23:41 +0000

    python script 2 get the zed depth image

---
+++ scripts/listener.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+import numpy as np
+import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+
+def listener():
+    rospy.init_node('listener')
+    image_topic = "/zed/depth/depth_registered"
+    rospy.Subscriber(image_topic, Image, image_callback)
+    rate = rospy.Rate(10) # 10hz
+    #rospy.spin()
+    while not rospy.is_shutdown():
+        rate.sleep()
+
+def image_callback(msg):
+    #print("Received an image!")
+    cv2_img = bridge.imgmsg_to_cv2(msg, "32FC1")
+    print(cv2_img[360,640])#1280x720
+    cv2.line(cv2_img,(639,359),(640,360),0,5)
+    cv2.imshow("raw",0.1*cv2_img)
+    cv2.waitKey(1)
+
+if __name__ == '__main__':
+    bridge = CvBridge()
+    listener()
