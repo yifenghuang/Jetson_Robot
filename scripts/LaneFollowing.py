5ca088775f3bb15778d1f044c56c5eaab896ee52
hyf <yifenghuang.dh@gmail.com>
2018-02-05 07:18:48 +0000

    basic following line

--- scripts/LaneFollowing.py
+++ scripts/LaneFollowing.py
@@ -3,6 +3,7 @@
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Empty
+from std_msgs.msg import Int16MultiArray
 import numpy as np
 import cv2
 from sensor_msgs.msg import Image
@@ -42,9 +43,9 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
 def listener():
-    rospy.init_node('ZED')
-    
-    image_topic = "/zed/left/image_raw_color"
+    #rospy.init_node('ZED')
+    rospy.init_node('LaneFollowing')
+    image_topic = "/zed/right/image_raw_color"
     rospy.Subscriber(image_topic, Image, image_callback)
     rate = rospy.Rate(10) # 10hz
     #rospy.spin()
@@ -59,15 +60,15 @@ def image_callback(msg):
     G = cv2_img[:,:,1] 
     R = cv2_img[:,:,0]
 
-    thresh = (0, 45)
+    thresh = (0, 35)
     RGBtb = np.zeros_like(B)
     RGBtb[(B > thresh[0]) & (B <= thresh[1])] = 1
 
-    thresh = (0, 45)
+    thresh = (0, 35)
     RGBtg = np.zeros_like(B)
     RGBtg[(G > thresh[0]) & (G <= thresh[1])] = 1
 
-    thresh = (0, 45)
+    thresh = (0, 35)
     RGBtr = np.zeros_like(B)
     RGBtr[(R > thresh[0]) & (R <= thresh[1])] = 1
 
@@ -82,29 +83,43 @@ def image_callback(msg):
    # cv2.imshow("raw",edges)
     imshape = edges.shape
 
-    vertices = np.array([[(0,imshape[0]),(228, 49), (511,49), (imshape[1],imshape[0])]], dtype=np.int32)
+    vertices = np.array([[(100,imshape[0]),(263, 49), (445,49), (imshape[1],imshape[0])]], dtype=np.int32)
     masked_edges = region_of_interest(out_img, vertices)
 
 
-    midpoint = np.int(masked_edges.shape[1]/2)
+    midpoint = np.int(masked_edges.shape[1]/2) + 91 #offset
     point_row = 188 # set which row you want to find the point
-    margin = 30 # set the window size of the windows +/- margin
-    minpix = 15 # set the minimun number of pixels found to recenter window
+    margin = 40 # set the window size of the windows +/- margin
+    minpix = 12 # set the minimun number of pixels found to recenter window
 
     black = midpoint
     for window in range(masked_edges.shape[1]-margin):        
         if np.sum(masked_edges[point_row][window:(window+margin)]) > minpix:
-            black = window
+            black = window+margin/2
             break
 
-    print(black)
-    cv2.imshow("raw",255*masked_edges)
+    img_forshow = np.zeros_like(cv2_img)
+    img_forshow[:,:,2] = 255*masked_edges
+    img_forshow[:,:,1] = 255*masked_edges
+    img_forshow[:,:,0] = 255*masked_edges
+    cv2.line(img_forshow,(black,point_row),(black+margin,point_row),(255,0,0),5)
+
+    throttle = 16
+
+    speed_data = Int16MultiArray()
+    speed_data.data = [throttle + 0.03*(black-midpoint), throttle - 0.03*(black-midpoint)]
+    rospy.loginfo(speed_data)
+    pub_speed.publish(speed_data)
+
+
+    cv2.imshow("raw",img_forshow)
 
     cv2.waitKey(1)
 
 if __name__ == '__main__':
     bridge = CvBridge()
     pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub_speed = rospy.Publisher('speed', Int16MultiArray, queue_size=1)
     pub_obstacle = rospy.Publisher('obstacle', Empty, queue_size=10)
-    pub_ok = rospy.Publisher('ok', Empty, queue_size=10)
+    #pub_ok = rospy.Publisher('ok', Empty, queue_size=10)
     listener()
--- scripts/listener.py
+++ scripts/listener.py
@@ -3,7 +3,7 @@
 
 import rospy
 from std_msgs.msg import Int16MultiArray
-import urllib
+import urllib2
 import json
 
 dic = {}
@@ -16,13 +16,14 @@ def callback(data):
     num+=1
 
 def get_cordinate():
-    r=urllib.request.urlopen('http://192.168.1.5:8888/position.htm?eid=26E0')
+    r=urllib2.urlopen('http://192.168.1.5:8888/position.htm?eid=26E0')
     j=json.loads(r.read())
     return j["body"]["x"], j["body"]["y"]
 
 def write_js(left, right, time, num):
-    #x,y=get_cordinate()
-    test_dict = {num: [['x', 1], ['y', 2], ['left', left], ['right', right], ['time', time]]}
+    x,y=get_cordinate()
+    print x,y
+    test_dict = {num: [['x', x], ['y', y], ['left', left], ['right', right], ['time', time]]}
     dic.update(test_dict)
     json_str = json.dumps(dic)
     new_dict = json.loads(json_str)
@@ -36,7 +37,7 @@ def listener():
 
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("monitorSpeed", Int16MultiArray, callback)
+    rospy.Subscriber("monitor", Int16MultiArray, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
